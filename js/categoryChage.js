3b9a14e27894caf8b2f0e9e4c2d7768b9a006052
sciqita <97511896+sciqita@users.noreply.github.com>
2022-02-25 11:24:39 +0900

    Add files via upload

---
+++ js/categoryChage.js
@@ -0,0 +1,61 @@
+let btnAll = document.getElementById("btnAll");
+let btnOrgn = document.getElementById("btnOrgn");
+let btnFncy = document.getElementById("btnFncy");
+let btnAntq = document.getElementById("btnAntq");
+let imgs = document.getElementById("GalleryImgs");
+let imgOrgn = document.getElementsByClassName("imgOrgn");
+let imgFncy = document.getElementsByClassName("imgFncy");
+let imgAntq = document.getElementsByClassName("imgAntq");
+
+btnAll.onclick = function() {
+	btnAll.classList.add("active")
+  btnOrgn.classList.add("active")
+  btnFncy.classList.add("active")
+  btnAntq.classList.add("active")
+};
+btnOrgn.onclick = function() {
+	btnOrgn.classList.add("active")
+  btnAll.classList.remove("active")
+  btnFncy.classList.remove("active")
+  btnAntq.classList.remove("active")
+};
+btnFncy.onclick = function() {
+	btnFncy.classList.add("active")
+  btnAll.classList.remove("active")
+  btnOrgn.classList.remove("active")
+  btnAntq.classList.remove("active")
+};
+btnAntq.onclick = function() {
+	btnAntq.classList.add("active")
+  btnAll.classList.remove("active")
+  btnOrgn.classList.remove("active")
+  btnFncy.classList.remove("active")
+};
+
+document.onclick = function() {
+  if(btnOrgn.classList.contains("active") == true) {
+    for(i=0;i<imgOrgn.length;i++){
+      imgOrgn[i].style.display = "block";
+    }} else {
+    for(i=0;i<imgOrgn.length;i++){
+      imgOrgn[i].style.display = "none";
+    }}
+};
+document.onclick = function() {
+  if(btnFncy.classList.contains("active") == true) {
+    for(i=0;i<imgFncy.length;i++){
+      imgFncy[i].style.display = "block";
+    }} else {
+    for(i=0;i<imgFncy.length;i++){
+      imgFncy[i].style.display = "none";
+    }}
+};
+document.onclick = function() {
+  if(btnAntq.classList.contains("active") == true) {
+    for(i=0;i<imgAntq.length;i++){
+      imgAntq[i].style.display = "block";
+    }} else {
+    for(i=0;i<imgAntq.length;i++){
+      imgAntq[i].style.display = "none";
+    }}
+};
